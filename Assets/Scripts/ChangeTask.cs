07967f6aa1db3a912ba231d18c192d4daa0d37cb
DesLigaFacens <69490107+MRJackFrost@users.noreply.github.com>
2024-06-06 17:17:06 -0300

    Fix(PerguntasAlteraveis)

--- Assets/Scripts/ChangeTask.cs
+++ Assets/Scripts/ChangeTask.cs
@@ -11,7 +11,27 @@ public class ChangeTask : MonoBehaviour
     public TMP_Text task3;
     public TMP_Text task4;
     public TMP_Text button;
+    
+    public string inputtask1 = "Siga nossa pagina no Instagram";
+    public string inputtask2 = "Siga nossa Pagina no X/Twitter";
+    public string inputtask3 = "Assista a apresentacao por completo";
+    public string inputtask4 = "Chame um amigo para participar";
+    
+    public string protask1 = "willson";
+    public string protask2 = "willson";
+    public string protask3 = "willson";
+    public string protask4 = "willson";
 
+    public TMP_InputField in1;
+    public TMP_InputField in2;
+    public TMP_InputField in3;
+    public TMP_InputField in4;
+    
+    public TMP_InputField prin1;
+    public TMP_InputField prin2;
+    public TMP_InputField prin3;
+    public TMP_InputField prin4;
+    
     public bool professores;
     void Start()
     {
@@ -26,19 +46,17 @@ public class ChangeTask : MonoBehaviour
 
     public void ChangeTeacher()
     {
-        task1.text = "O professor Wilson deve dar nota a mais por esta tarefa";
-        task2.text = "O professor Wilson tem que dar 100 conto no pix para cada membro";
-        task3.text = "O professor Wilson deve pegar leve na avaliacao de modelagem";
-        task4.text = "acabou as ideias para o professor Wilson";
-        button.text = "VISITANTES";
+        task1.text = protask1;
+        task2.text = protask2;
+        task3.text = protask3;
+        task4.text = protask4;
     }
     public void ChangeNormal()
     {
-        task1.text = "Siga nossa pagina no Instagram";
-        task2.text = "Siga nossa Pagina no X/Twitter";
-        task3.text = "Assista a apresentacao por completo";
-        task4.text = "Chame um amigo para participar";
-        button.text = "PROFESSORES";
+        task1.text = inputtask1;
+        task2.text = inputtask2;
+        task3.text = inputtask3;
+        task4.text = inputtask4;
     }
 
     public void ToChange()
@@ -52,5 +70,22 @@ public class ChangeTask : MonoBehaviour
             ChangeNormal();
             professores = false;
         }
+    }
+
+    public void ChangeTextNormal()
+    {
+        inputtask1 = in1.text;
+        inputtask2 = in2.text;
+        inputtask3 = in3.text;
+        inputtask4 = in4.text;
+        ChangeNormal();
+    }
+    public void ChangeTextProfessor()
+    {
+        protask1 = prin1.text;
+        protask2 = prin2.text;
+        protask3 = prin3.text;
+        protask4 = prin4.text;
+        ChangeTeacher();;
     }
 }
